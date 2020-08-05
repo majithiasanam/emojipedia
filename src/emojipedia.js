72767783f7e91047d95c270a2d68a9f41d74e53c
Sanam MAJITHIA <sanam.majithia@amadeus.com>
2020-08-06 02:21:17 +0530

    Added more emojis

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -19,6 +19,79 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🐶",
+    name: "Dog Face",
+    meaning:
+      "A sweet dog face with a panting tongue and floppy ears. Stands for unconditional loyalty and attention. Can refer to your own pet (“I love my dog.“) or to people (“You sly dog!“)."
+  },
+  {
+    id: 5,
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
+    meaning: "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things."
+  },
+  {
+    id: 6,
+    emoji: "🐣",
+    name: "Chick in Eggshell",
+    meaning: "A sweet fledgling is hatching from its egg. Popular symbol for Easter. Stands for recommencement, cuteness or the birth of a baby."
+  },
+  {
+    id: 7,
+    emoji: "🦄",
+    name: "Unicorn",
+    meaning: "The unicorn: a white horse with magical powers and a horn on its forehead. The mythical creatures represent uniqueness, innocence, virginity, and childlike naivety. Symbol for the good."
+  },
+  {
+    id: 8,
+    emoji: "🍏",
+    name: "Green Apple",
+    meaning: "Logo of one of the most well-known companies. “An apple a day keeps the doctor away.“ Represents health and is the Germans' favorite fruit. The green apple is also a symbol of friendship."
+  },
+  {
+    id: 9,
+    emoji: "🍇",
+    name: "Grapes",
+    meaning: "The grapes are hanging too high, wishes are unattainable or goals unreachable. The grape is traditionally interpreted as a sign of good health. Christian symbol and raw material for wine."
+  },
+  {
+    id: 10,
+    emoji: "🧀",
+    name: "Chunk of Cheese",
+    meaning: "I love cheese or “Hard cheese!“ Can also be used for dairy products in general. “Cheese“: Standard command of the photographer for everyone to smile on the picture."
+  },
+  {
+    id: 11,
+    emoji: "🍭",
+    name: "Lollipop",
+    meaning: "You're so sweet! or “I want candy!“ The lollipop conveys fun and reminds us of our childhood. Also used to conciliate a person or in a sexual context. Popular emoji of food bloggers."
+  },
+  {
+    id: 12,
+    emoji: "⚽",
+    name: "Soccer Ball",
+    meaning: "I want to play soccer or “The game is starting!“ The black-and-white ball is mostly used in conjunction with European soccer."
+  },
+  {
+    id: 13,
+    emoji: "🏆",
+    name: "Cup",
+    meaning: "A shiny cup, usually with an engraving. A symbol of success and recognition of the performance of an individual or a team. We won!"
+  },
+  {
+    id: 14,
+    emoji: "🚑",
+    name: "Ambulance",
+    meaning: "An ambulance transports non-critically ill people to a hospital. On the other hand, a mobile intensive care unit is usedforemergency ambulance service.Emoji is related to injuries, illnesses, accidents and medicaltopics."
+  },
+  {
+    id: 15,
+    emoji: "🎉",
+    name: "Party Popper",
+    meaning: "Happy Birthday! The confetti bomb explodes in a fountain of paper streamers and confetti. A party might be in progress or someone has been successful and is very happy about something."
   }
 ];
 
