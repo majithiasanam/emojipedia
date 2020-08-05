abd528fd59dc3ff8f060931cb56e631cd719dae4
Sanam MAJITHIA <sanam.majithia@amadeus.com>
2020-08-06 01:58:55 +0530

    Updates

---
+++ precache-manifest.fd86d9ca74bf3fa78f1d2b1542e9a95a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3147bd4f201255c6ab9ff6448bfe04ca",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "e74e4fbbdffd3d5d1653",
+    "url": "/emojipedia/static/js/2.4e181e1c.chunk.js"
+  },
+  {
+    "revision": "cb45a811616cd7272acf",
+    "url": "/emojipedia/static/js/main.824b22af.chunk.js"
+  },
+  {
+    "revision": "466c8a8b5c549047fcd8",
+    "url": "/emojipedia/static/js/runtime-main.5e104e69.js"
+  }
+]);
